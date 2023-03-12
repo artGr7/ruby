1e1c3a8d6765eb6e8f806534fd725f55bac2aafd
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-12 19:43:53 +0300

    Add files via upload

---
+++ 1lab/3.2.rb
@@ -0,0 +1,32 @@
+def min(a)
+  minimal = a[0]
+  for i in a do
+    if i<minimal
+      minimal=i
+    end
+  end
+  return minimal
+end
+
+def index_of_first_positive(a)
+  for i in a do
+    if (i>=0)
+      break
+    end
+  end
+  return (a.index(i))
+end
+
+def action(num, path)
+  file = File.open(path)
+  arr = file.read.split.map {|element| elements.to_i}
+  if (num.to_i == 1)
+    puts (min(arr))
+  else
+    puts (index_of_first_positive(arr))
+  end
+end
+
+num = ARGV[0]
+path = ARGV[1]
+action(num,path)
