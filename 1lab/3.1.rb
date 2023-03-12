1279e536247164a2b458f86571dbbbf9edc6ec53
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-12 18:25:43 +0300

    Add files via upload

---
+++ 1lab/3.1.rb
@@ -0,0 +1,21 @@
+def min(a)
+  minimal = a[0]
+  for i in a do
+    if i<minimal
+      minimal=i
+    end
+  end
+  return minimal
+end
+
+def index_of_first_positive(a)
+  for i in a do
+    if (i>=0)
+      break
+    end
+  end
+  return (a.index(i))
+end
+
+a=[-2, -3, -3, 3, 4]
+puts min(a).to_s + " #{index_of_first_positive a}"
