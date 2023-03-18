5f4af7a6facb4d78a4a384c46ceb027c6ee721ea
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-18 10:10:05 +0300

    Update 1.1.rb

--- 2lab/1.1.rb
+++ 2lab/1.1.rb
@@ -1,6 +1,4 @@
 class Student
-  attr_accessor :phone, :id, :name, :lastName, :patronymic, :git, :telegram, :email
-
   def initialize(name, lastName, patronymic)
     @id
     @lastName = lastName
@@ -11,4 +9,83 @@ class Student
     @email
     @git
   end
+  
+  
+  def getid
+		@id
+  end
+  
+  
+	def setid(id)
+		@id = id
+	end
+	
+  
+	def getname
+		@name
+	end
+	
+  
+  def setname(name)
+		@name = name
+	end
+	
+  
+	def getlastName
+		@lastName
+	end
+	
+  
+  def setlastName(lastName)
+		@lastName = lastName
+	end
+	
+	def getlastName
+		@lastName
+	end
+	
+  
+  def setlastName(lastName)
+		@lastName = lastName
+	end
+
+  
+	def getphone
+		@phone
+	end
+	
+  
+  def setphone(phone)
+		@phone = phone
+	end
+	
+	
+  def gettelegram
+		@telegram
+	end
+  
+  
+	def settelegram(telegram)
+		@telegram = telegram
+	end
+	
+	def getemail
+		@email
+	end
+  
+  
+	def setemail(email)
+		@email = email
+	end
+	
+	def getgit
+		@git
+	end
+  
+  
+	def setgit(git)
+		@git = git
+	end
+  
+  
 end
