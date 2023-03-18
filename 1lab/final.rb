c7188f0abcbd70b8b3525373e4c0e77ea95e93e9
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-18 13:27:08 +0300

    Update final.rb

--- 1lab/final.rb
+++ 1lab/final.rb
@@ -1,24 +1,24 @@
-def TASK8(arr) # Дан целочисленный массив. Необходимо найти индексы двух наименьших элементов массива.
+def two_min(arr) # Дан целочисленный массив. Необходимо найти индексы двух наименьших элементов массива.
   a = arr.min(2)
   arr.index(a[0]).to_s + " #{arr.index(a[1]).to_s}"
 end
 
-def TASK20(arr) # Дан целочисленный массив. Необходимо найти все пропущенные числа.
+def missed_nums(arr) # Дан целочисленный массив. Необходимо найти все пропущенные числа.
   new_array = Array(arr.min..arr.max)
   new_array - arr
 end
 
-def TASK32(arr) # Дан целочисленный массив. Найти количество его локальных максимумов.
+def local_max(arr) # Дан целочисленный массив. Найти количество его локальных максимумов.
   arr.select.with_index {|el, i| i!=0 and i!=arr.length-1 and arr[i]>arr[i+1] and arr[i]>arr[i-1]}.length
 end
 
-def TASK44(array) # Дан массив чисел. Необходимо проверить, чередуются ли в нем целые и вещественные числа.
+def flo_int(array) # Дан массив чисел. Необходимо проверить, чередуются ли в нем целые и вещественные числа.
   int = array[0].kind_of?(Integer) ? Integer : Float
   new_a = array.partition.with_index{|i,ind| ind.even?}
   new_a[0].all?{|i| i.kind_of? int} and new_a[1].all?{|i| i.kind_of? int}
 end
 
-def TASK56(arr) # Для введенного списка посчитать среднее арифметическое непростых элементов, которые больше, чем среднее арифметическое простых.
+def meanprime(arr) # Для введенного списка посчитать среднее арифметическое непростых элементов, которые больше, чем среднее арифметическое простых.
   require 'prime'
   prime_arr = arr.keep_if { |el| el.prime? == true}
   prime_avg = prime_arr.inject { |sum, el| sum + el }.to_f / arr.size
@@ -33,15 +33,15 @@ def complete(path)
   numb = STDIN.gets.to_i
   case numb
     when 8
-      p TASK8(data)
+      p two_min(data)
     when 20
-      p TASK20(data)
+      p missed_nums(data)
     when 32
-      p TASK32(data)
+      p local_max(data)
     when 44
-      p TASK44(data)
+      p flo_int(data)
     when 56
-      p TASK56(data)
+      p meanprime(data)
     else
       puts "Ты где тут нашел #{numb}???"
   end
