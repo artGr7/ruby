ada0d5db3350c32558702ba6df7a9e2741391b3d
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-18 09:08:11 +0300

    Create 1.1.rb

---
+++ 2lab/1.1.rb
@@ -0,0 +1,14 @@
+class Student
+  attr_accessor :phone, :id, :name, :lastName, :patronymic, :git, :telegram, :email
+
+  def initialize(name, lastName, patronymic)
+    @id
+    @lastName = lastName
+    @name = name
+    @patronymic = patronymic
+    @phone
+    @telegram
+    @email
+    @git
+  end
+end
