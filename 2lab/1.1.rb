929a498f2627bf2812e85d99afa6d5aa3ad910f6
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-18 10:12:28 +0300

    Update 1.1.rb

--- 2lab/1.1.rb
+++ 2lab/1.1.rb
@@ -10,82 +10,68 @@ class Student
     @git
   end
   
-  
   def getid
-		@id
+    @id
   end
   
+  def setid(id)
+    @id = id
+  end
   
-	def setid(id)
-		@id = id
-	end
-	
-  
-	def getname
-		@name
-	end
-	
-  
-  def setname(name)
-		@name = name
-	end
-	
-  
-	def getlastName
-		@lastName
-	end
-	
-  
-  def setlastName(lastName)
-		@lastName = lastName
-	end
-	
-	def getlastName
-		@lastName
-	end
-	
-  
-  def setlastName(lastName)
-		@lastName = lastName
-	end
+  def getname
+    @name
+  end
 
+  def setname(name)
+    @name = name
+  end
+
+  def getlastName
+    @lastName
+  end
   
-	def getphone
-		@phone
-	end
-	
+  def setlastName(lastName)
+    @lastName = lastName
+  end
+  
+  def getlastName
+    @lastName
+  end
+  
+  def setlastName(lastName)
+    @lastName = lastName
+  end
+  
+  def getphone
+    @phone
+  end
   
   def setphone(phone)
-		@phone = phone
-	end
-	
-	
+    @phone = phone
+  end
+  
   def gettelegram
-		@telegram
-	end
+    @telegram
+  end
   
+  def settelegram(telegram)
+    @telegram = telegram
+  end
+
+  def getemail
+    @email
+  end
   
-	def settelegram(telegram)
-		@telegram = telegram
-	end
-	
-	def getemail
-		@email
-	end
+  def setemail(email)
+    @email = email
+  end
+
+  def getgit
+    @git
+  end
   
-  
-	def setemail(email)
-		@email = email
-	end
-	
-	def getgit
-		@git
-	end
-  
-  
-	def setgit(git)
-		@git = git
-	end
-  
+  def setgit(git)
+    @git = git
+  end
   
 end
