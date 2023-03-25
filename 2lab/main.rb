f926991cc0acd5459588c6d05136dd3185079b25
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-03-25 08:39:15 +0300

    Create main.rb

---
+++ 2lab/main.rb
@@ -0,0 +1,23 @@
+load('1.2.rb')
+first_guy = Student.new("Бекеров", "Алиссон", "Бразильевич")
+first_guy.megaprint
+second_guy = Student.new("Трентов", "Саша-Арнольд", "Англиевич")
+second_guy.megaprint
+third_guy = Student.new("Гомезов", "Джо", "Англиевич")
+third_guy.megaprint
+fourth_guy = Student.new("Вандайков", "Вирджил", "Голандович")
+fourth_guy.megaprint
+fifth_guy = Student.new("Робертсонов", "Эндрю", "Скотландович")
+fifth_guy.megaprint
+sixth_guy = Student.new("Нетфамилиев", "Фабиньо", "Бразильевич")
+sixth_guy.megaprint
+seventh_guy = Student.new("Хендерсонов", "Джордан", "Англиевич")
+seventh_guy.megaprint
+eighth_guy = Student.new("Милнеров", "Джеймс", "Англиевич")
+eighth_guy.megaprint
+nineth_guy = Student.new("Мохамедов", "Салах", "Египтович")
+nineth_guy.megaprint
+tenth_guy = Student.new("Нуньезов", "Дарвин", "Уругваевич")
+tenth_guy.megaprint
+eleventh_guy = Student.new("Гакпоев", "Коди", "Голандович")
+eleventh_guy.megaprint
