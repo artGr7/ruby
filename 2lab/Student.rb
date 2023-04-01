b35ec2305775ebbeadde486a9b4f01187b333b89
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-04-01 08:15:30 +0300

    Update Student.rb

--- 2lab/Student.rb
+++ 2lab/Student.rb
@@ -2,6 +2,8 @@ class Student
   attr_accessor :id, :lastName, :name, :patronymic, :phone, :telegram, :email, :git
 
   def initialize(lastName: , name: , patronymic: , id: nil, phone: nil, telegram: nil, email: nil, git: nil)
+    if phone != nil then
+      check_phone(phone)
     self.id= id
     self.lastName= lastName
     self.name= name
@@ -10,10 +12,17 @@ class Student
     self.telegram= telegram
     self.email= email
     self.git= git
+    end
   end
 
   def to_s
     "Id: #{@id}, Lastname: #{@lastName}, Name: #{@name}, Patronymic: #{@patronymic}, Phone: #{@phone}, Telegram #{@telegram}, Email #{@email}, Github link: #{@git}\n\n"
+  end
+
+  def check_phone(phone)
+    if phone !=~ /\A\+?\d{11}\z/
+      raise ArgumentError, "Неверный номер"
+    end
   end
 
 end
