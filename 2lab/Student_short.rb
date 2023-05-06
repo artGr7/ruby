d99f94344aa2296b5ff9da8c70a096fcb770f053
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-05-06 13:03:15 +0300

    Update Student_short.rb

--- 2lab/Student_short.rb
+++ 2lab/Student_short.rb
@@ -1,18 +1,27 @@
-class Student_short
-  attr_reader :id, :git, :initials, :contact
-
-  def initialize(student)
-    self.id = student.id
-    self.initials = student.short_name
-    self.git = student.git
-    self.contact = student.set_contacts(phone: student.phone, mail: student.email, tme: student.telegram)
+load('Abstract_student.rb')
+load('student.rb')
+class Student_short < Abstract_student
+  attr_reader :id, :initials, :git, :contact
+  def initialize(id:, initials:, git:, contact:)
+    @id = id
+    @initials = initials
+    @git = git
+    @contact = contact
   end
 
-  def self.from_s(str)
+  def self.from_student(student)
+    Student_short.new(id: student.id, initials: "#{student.lastName} #{student.name[0]}. #{student.patronymic[0]}.", git: student.git, contact: student.random_contact)
+  end
+
+  def self.some_contact(hash)
+    if hash["phone"] then return hash["phone"] end
+    if hash["email"] then return hash["email"] end
+    if hash["telegram"] then return hash["telegram"] end
+    return " "
+  end
+
+  def self.from_s(str, id)
     hash = str.split(', ').map{|el| el.split(': ')}.to_h
-    Student_short.new(initials: hash["initials"], git: hash["git"], contact: hash["contact"])
+    Student_short.new(id: id, initials: hash["initials"], git: hash["git"], contact: self.some_contact(hash))
   end
-
-  def to_s
-    "id: #{@id}, name: #{@initials}, contact: #{@contact}, git: #{@git}"
-  end
+end
