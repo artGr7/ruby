a4bfa48e6ddd8b4d3cc5409c0b141f3a758539c8
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-05-11 17:38:31 +0300

    Update Student_short.rb

--- 2lab/Student_short.rb
+++ 2lab/Student_short.rb
@@ -1,6 +1,5 @@
-load('Abstract_student.rb')
 load('student.rb')
-class Student_short < Abstract_student
+class Student_short < Student
   attr_reader :id, :initials, :git, :contact
   def initialize(id:, initials:, git:, contact:)
     @id = id
@@ -9,7 +8,11 @@ class Student_short < Abstract_student
     @contact = contact
   end
 
-  def self.from_student(student)
+  def to_s
+    "Id: #{@id}, Initials: #{@initials}, Contact: #{@contact}, Github link: #{@git}"
+  end
+
+  def self.from_student (student)
     Student_short.new(id: student.id, initials: "#{student.lastName} #{student.name[0]}. #{student.patronymic[0]}.", git: student.git, contact: student.random_contact)
   end
 
@@ -22,6 +25,15 @@ class Student_short < Abstract_student
 
   def self.from_s(str, id)
     hash = str.split(', ').map{|el| el.split(': ')}.to_h
-    Student_short.new(id: id, initials: hash["initials"], git: hash["git"], contact: self.some_contact(hash))
+    if git_valid?(hash["git"])
+      Student_short.new(id: id, initials: hash["initials"], git: hash["git"], contact: self.some_contact(hash))
+    end
   end
 end
+
+first_guy = Student.new(lastName: "Кто", name: "Яя", patronymic: "Ты", phone: "89182338282", git: "sddsd")
+puts Student_short.from_student(first_guy)
+short_guy = Student_short.from_student(first_guy)
+puts short_guy.contact
+short_guy2 = Student_short.from_s("initials: Кто Я. Т., phone: 89182338282, git: хуй", 1)
+puts short_guy2
