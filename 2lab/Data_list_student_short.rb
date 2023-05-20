ea3bd751eb29523ca1f6c82d2f13596b15c640d9
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-05-20 13:26:05 +0300

    Update Data_list_student_short.rb

--- 2lab/Data_list_student_short.rb
+++ 2lab/Data_list_student_short.rb
@@ -9,21 +9,10 @@ class Data_list_student_short < Data_list
     @@number+=1
     str=data[0]
     hash = str.split(', ').map{|el| el.split(': ')}.to_h
-    attributes = []
-    get_names.each {|el| attributes << hash[el]}
-    Data_table.new([@@number]+attributes)
+    Data_table.new([@@number]+[hash["Initials"]]+[hash["Contact"]]+ [hash["Github link"]])
   end
 
   def get_names
-    str=data[0]
-    hash = str.split(', ').map{|el| el.split(': ')}.to_h
-    hash.keys[1...hash.keys.length]
+    ["ФИО","Контакт","Гит"]
   end
-
 end
-
-short_guy2 = Student_short.from_s("initials: Кто Я. Т., phone: 89182338282, git: sddd", 1)
-datalist_guy = Data_list_student_short.new([short_guy2.to_s])
-datalist_guy2 = Data_list_student_short.new([short_guy2.to_s])
-p datalist_guy.get_data.data
-p datalist_guy2.get_data.data
