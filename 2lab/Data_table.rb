ebf40bc521824ebf3798a7f7babe448c779a6274
artGr7 <96309384+artGr7@users.noreply.github.com>
2023-05-20 13:00:45 +0300

    Create Data_table.rb

---
+++ 2lab/Data_table.rb
@@ -0,0 +1,27 @@
+class Data_table
+
+  def initialize(data)
+    self.data= data
+  end
+
+  def at(row,col)
+    data[row][col]
+  end
+
+  def rows_length
+    data.length
+  end
+
+  def columns_length
+    data[0].length
+  end
+
+
+  def data=(data)
+    @data=data
+  end
+  def data
+    @data
+  end
+
+end
